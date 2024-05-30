4470494106fd170f25c36a1350a9a0ab81c3c1f5
Mattia Andreolli <mattia.andreolli1@gmail.com>
2024-05-30 13:41:55 +0200

    Added DS auth handler class

---
+++ SquadUp/SquadUp/Authentication/DiscordAuthenticationHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace SquadUp.Authentication
+{
+    public class DiscordAuthenticationHandler : SignInAuthenticationHandler<DiscordAuthenticationOptions>
+    {
+        //TODO Add services and vars
+        
+        public DiscordAuthenticationHandler(
+            IOptionsMonitor<DiscordAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder
+        )
+            : base(options, logger, encoder)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override async Task HandleSignOutAsync(AuthenticationProperties? properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override async Task HandleSignInAsync(ClaimsPrincipal user, AuthenticationProperties? properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SquadUp/SquadUp/Authentication/DiscordAuthenticationOptions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace SquadUp.Authentication
+{
+    /// <summary>
+    /// Class to hold options for Discord OAuth2 auth scheme
+    /// </summary>
+    public class DiscordAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        
+    }
+}
